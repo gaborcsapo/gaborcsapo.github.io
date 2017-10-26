bfa0bce7b29c2f0622f7062048439cf244cfd670
Gabor <gabor.csapo@nyu.edu>
2017-10-26 12:56:47 +0400

    optimized

--- js/blog.js
+++ js/blog.js
@@ -1,148 +1,36 @@
-/*jshint esversion: 6 */
-
-// polygon array and number of verts
-let poly = []
-let n = 100 // feel free to play with this number :)
-
-// canvas size variables
-let w = 200
-let h = 250
-
-// oscillators
-let chord = []
-let root = 30
-let major = [ 4, 5, 6 ]
-let minor = [ 10, 12, 15 ]
-
-// setup and draw functions ---
-
-function setup() {
-  var canvas = createCanvas(w, h)
-  canvas.parent('header-sketch');
-  strokeWeight(17)
-  noFill()
-  cursor(HAND)
-  noStroke()
-  n++ // add extra point for closing the polygon
-  
-  for (let i = 0; i < n; i++) {
-    // populate regular polygon vertices given number of points n
-    let a = {
-      x: (w/2)-10 + 64*sin(map(i, 0, n-1, 0, TAU)),
-      y: (h/2) + 64*cos(map(i, 0, n-1, 0, TAU))
-    }
-    poly.push(a)
-  }
-  
-  // initialize oscillators
-  if (n < 25) {
-    for (let i = 0; i < 3; i++)
-      chord[i] = new p5.TriOsc()
-  } else {
-    for (let i = 0; i < 3; i++)
-      chord[i] = new p5.SinOsc()
-  }
-  
-  // initialize with major chord intervals
-  for (let i = 0; i < chord.length; i++) {
-      chord[i].freq(major[i] * root)
-        chord[i].amp(0.0)
-      chord[i].stop()
-  }
-  $( ".header-text, #header-sketch" ).fadeIn( "slow")
-}
-
-function draw() {
-  // use default blend mode for background
-  blendMode(BLEND)
-  background(247, 247, 249)
-  
-  // use additive blend mode to separate color channels
-  blendMode(DIFFERENCE)
-  stroke(255, 0, 0)
-  drawPoly(1000, 1000)
-  
-  stroke(0, 255, 0)
-  drawPoly(1200, 1500)
-  
-  stroke(0, 0, 255)
-  drawPoly(2000, 1700)
-  
-  // distort oscillatiors
-  warpOsc()
-}
-
-
-// helper function implementations ---
-
-function logMap(value, start1, stop1, start2, stop2) {
-  // based off of linear regression + existing p5.map function
-  
-  start2 = log(start2)
-  stop2 = log(stop2)
- 
-  return exp(start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1)))
-}
-
-function drawPoly(dx, dy) {
-  // draws polygon given vertices in the poly[] array, adds mouse bias using params
-  
-  let g = 0
-  if (mouseIsPressed)
-    g = random(-2, 2)
-    
-  beginShape()
-  for (let i = 0; i < n; i++) {
-    let bias = dist(mouseX, mouseY, poly[i].x, poly[i].y)
-    vertex(poly[i].x + dx / logMap(bias, w, 0, dx, 45) + g, poly[i].y + dy / logMap(bias, h, 0, dy, 45) + g)
-  }
-  endShape()
-}
-
-function warpOsc() {
-  // uses max dist to determine the frequency distortion
-  
-  let bias = 0
-  for (let i = 0; i < n; i++)
-    bias = max(bias, dist(mouseX, mouseY, poly[i].x, poly[i].y))
-  
-  for (let i = 0; i < chord.length; i++){
-    var frequencyCalc = map(bias, w, 0, major[i], minor[i]) * root;
-    if (frequencyCalc < 22000 && frequencyCalc > -22000){
-      chord[i].freq(frequencyCalc)
-    }
-  }
-    
-}
-
-//Infinte scrolling:
-$(document).ready(function() {
-  var win = $(window);
-  var count = 2
-  if (window.location.pathname.split("/").pop().match(/\d+/g) != null) {
-      count = 0;
-  }
-  var timeout;
-
-  // Each time the user scrolls
-  win.scroll(function() {
-    // End of the document reached?
-    clearTimeout(timeout);  
-    timeout = setTimeout(function() {
-        if ($(document).height() - win.height() - 200 < win.scrollTop()) {
-          if (count > 0){
-            $.ajax({
-              url: "/pages/blog"+count+".html",
-              dataType: 'html',
-              success: function (data) {
-                newcontent=$(data).find('.contents'); 
-                $('#contentsWrapper').append(newcontent);
-                $('#'+count+'-page').addClass('disabled').attr('title', 'already loaded');
-                count--;
-              },
-            });
-          } 
-        }
-      }, 50);
-    });
-});
+let poly=[]
+let n=100
+let w=200
+let h=250
+let chord=[]
+let root=30
+let major=[4,5,6]
+let minor=[10,12,15]
+function setup(){var canvas=createCanvas(w,h)
+canvas.parent('header-sketch');strokeWeight(17)
+noFill()
+cursor(HAND)
+noStroke()
+n++
+for(let i=0;i<n;i++){let a={x:(w/2)-10+64*sin(map(i,0,n-1,0,TAU)),y:(h/2)+64*cos(map(i,0,n-1,0,TAU))}
+poly.push(a)}if(n<25){for(let i=0;i<3;i++)chord[i]=new p5.TriOsc()}else{for(let i=0;i<3;i++)chord[i]=new p5.SinOsc()}for(let i=0;i<chord.length;i++){chord[i].freq(major[i]*root)
+chord[i].amp(0.0)
+chord[i].stop()}$(".header-text, #header-sketch").fadeIn("slow")}function draw(){blendMode(BLEND)
+background(247,247,249)
+blendMode(DIFFERENCE)
+stroke(255,0,0)
+drawPoly(1000,1000)
+stroke(0,255,0)
+drawPoly(1200,1500)
+stroke(0,0,255)
+drawPoly(2000,1700)
+warpOsc()}function logMap(value,start1,stop1,start2,stop2){start2=log(start2)
+stop2=log(stop2)
+return exp(start2+(stop2-start2)*((value-start1)/(stop1-start1)))}function drawPoly(dx,dy){let g=0
+if(mouseIsPressed)g=random(-2,2)
+beginShape()
+for(let i=0;i<n;i++){let bias=dist(mouseX,mouseY,poly[i].x,poly[i].y)
+vertex(poly[i].x+dx/logMap(bias,w,0,dx,45)+g,poly[i].y+dy/logMap(bias,h,0,dy,45)+g)}endShape()}function warpOsc(){let bias=0
+for(let i=0;i<n;i++)bias=max(bias,dist(mouseX,mouseY,poly[i].x,poly[i].y))
+for(let i=0;i<chord.length;i++){var frequencyCalc=map(bias,w,0,major[i],minor[i])*root;if(frequencyCalc<22000&&frequencyCalc>-22000){chord[i].freq(frequencyCalc)}}}$(document).ready(function(){var win=$(window);var count=2
+if(window.location.pathname.split("/").pop().match(/\d+/g)!=null){count=0;}var timeout;win.scroll(function(){clearTimeout(timeout);timeout=setTimeout(function(){if($(document).height()-win.height()-200<win.scrollTop()){if(count>0){$.ajax({url:"/pages/blog"+count+".html",dataType:'html',success:function(data){newcontent=$(data).find('.contents');$('#contentsWrapper').append(newcontent);$('#'+count+'-page').addClass('disabled').attr('title','already loaded');count--;},});}}},50);});});
---
+++ js/scripts-nonminified.js
@@ -0,0 +1,149 @@
+var windowHalfX = window.innerWidth / 2,
+windowHalfY = window.innerHeight / 2,
+mouseX = 0, 
+mouseY = 0,
+targetM = 0,
+globalmove = -1,
+globalpos = 10,
+offsets = [],
+order = 0,
+count,
+random = [0,0,0],
+local = [0,0,0],
+camera, scene, renderer;
+init();
+animate();
+
+function init() {
+	var container = document.getElementById('anim');
+	camera = new THREE.PerspectiveCamera( 75, container.offsetWidth / container.offsetHeight, 1, 10000 );
+	camera.position.z = 8;
+	scene = new THREE.Scene();
+	renderer = new THREE.CanvasRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize(container.offsetWidth, container.offsetHeight);
+	renderer.setClearColor( 0xf6e60a, 1);
+	container.appendChild( renderer.domElement );
+	
+	var radius = 5;
+	var segments = 100;
+	var material = new THREE.LineBasicMaterial( { color: 0x000000 } );
+	var geometry = new THREE.CircleGeometry( radius, segments );
+	var position = [];
+	geometry.vertices.shift();
+	for (var i = 9; i > 0; i--) {
+	  	circle = new THREE.Line(geometry, material);
+	  	circle.position.x = (Math.random()-0.5)/1.5;
+		circle.position.y = (Math.random()-0.5)/1.5;
+		scene.add(circle);
+	}
+	window.addEventListener( 'resize', onWindowResize, false );	
+}
+
+
+//if I hover over the vertical div, then the circle should do the global big move
+$( ".mouseover" )
+  .mouseenter(function() {
+  	if (order == 0){
+  		count = 0
+  		order = 1
+  	}
+  })
+  .mouseleave(function() {
+	if (order == 0){  	
+	  	count = 0
+	  	order = -1
+		for (var l = 8; l >= 0; l--) {
+			offsets[l] = [(Math.random()-0.5)/1.5, (Math.random()-0.5)/1.5]
+		}
+	}
+});
+
+function onWindowResize() {
+	var container = document.getElementById('anim');
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+	camera.aspect = container.offsetWidth / container.offsetHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( container.offsetWidth, container.offsetHeight );
+}
+
+//animation of the circles
+function animate() {
+	//lower framerate
+	setTimeout( function() {
+        requestAnimationFrame( animate );
+    }, 1000 / 26);
+	//moving the circles either in by a large or by small random amounts
+	if (globalmove !== 0) {
+		globalpos += ( targetM -  globalpos) * 0.05;
+		if (Math.abs(targetM -  globalpos) < 0.005) {
+			globalmove = 0;
+		}
+	} else {
+		for (var k = 2; k >= 0; k--) {	
+			if (Math.abs(random[k] - local[k]) < 0.01)
+				random[k] = (Math.random()-0.5)*2;
+			local[k] += (random[k] - local[k])*0.05;
+		}
+	}
+	//updating the location of each circle
+	for (var l = 2; l >= 0; l--) {
+		scene.children[l].position.z = globalpos + local[l];
+		scene.children[l+3].position.z = globalpos*(l+3) - 0.7 * local[l];
+		scene.children[l+6].position.z = globalpos*(l+6) + 0.5 * local[l];
+	}
+
+
+	//centering the circle
+	if (order == 1) {
+		if (count < 4){
+			count++;
+			for (var l = 8; l >= 0; l--) {
+				scene.children[l].position.x -= scene.children[l].position.x * 0.25;
+				scene.children[l].position.y -= scene.children[l].position.y * 0.25;
+			}
+		} else {
+			order = 0;
+		}
+	} else if (order == -1) {
+		if (count < 4){
+			count++;
+			for (var l = 8; l >= 0; l--) {
+				scene.children[l].position.x += offsets[l][0] * 0.25;
+				scene.children[l].position.y += offsets[l][1] * 0.25;
+			}
+		} else {
+			order = 0;
+		}
+	}
+
+	renderer.render( scene, camera );
+}
+
+
+//Color changing header and titles
+colors = ['#FFE82C','#F1C40F', '#8DC85E', '#27AE60', '#1ABC9C', '#A6DBD6', '#93AED4', '#00669F', '#9B59B6', '#EB4E32', '#F47B6C', '#FBCBC5', '#AC8B67'];
+var items = document.querySelectorAll('.color'),
+currentColorIndex = 0,
+totalColors = colors.length;
+
+for (var i = 0; i < items.length; i++) {
+	items[i].addEventListener('click', function() {
+	    currentColorIndex = (currentColorIndex + 1)%totalColors;
+	    currentColor = colors[currentColorIndex];
+	    renderer.setClearColor( currentColor, 1);
+	    $('.color')
+	    	.css('background-color', '')
+		    .css('background-color', currentColor);
+	});
+}
+
+//Smooth sroll
+var $root = $('html, body');
+$('a').click(function() {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
